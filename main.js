b652b20a7b2f9b242964c92ec5cc53a0b33e4b1d
Mniok <58875711+Mniok@users.noreply.github.com>
2022-07-23 23:00:16 +0200

    create app, display data
    newest version of vue included, vue app created and mounted, product and description displayed

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+//const product = 'Socks'
+
+const app = Vue.createApp({ //{"options object"} zawierający tylko 1 opcję - data.
+	data: function() {
+		return {
+			product: 'Socks',
+			description: 'As socky as can be. $15 per pair. If you cant afford them, socks to be you.'
+		} //zwraca {obiekt} typu data
+	}
+})
+
+// {} to obiekt btw
+
+
+//vue jest reaktywne - zmiana 'Socks' na np. 'Boots' sama updateuje się przy refreshu strony
+// w konsoli: mountedApp.product = 'nowa wartość'
