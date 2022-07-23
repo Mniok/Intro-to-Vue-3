6d241db6787689fe0e5aa1835961b0c994d3293b
Mniok <58875711+Mniok@users.noreply.github.com>
2022-07-24 00:42:45 +0200

    L4 finished - v-else-if, in stock / sold out + on sale

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-			inStock: true
+			//inStock: true,
+			inventory: 10,
+			onSale: true
         }
     }
 })
