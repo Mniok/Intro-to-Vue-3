abd3325e437f029fb76189c23bf6ef979dd0ae62
Mniok <58875711+Mniok@users.noreply.github.com>
2022-07-24 23:58:01 +0200

    L6 events - v-on (@) buttons, variants, add to cart
    add to cart, decrement cart, switch images on hover

--- main.js
+++ main.js
@@ -11,5 +11,22 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
-    }
+    },
+	
+	methods: {
+		addToCart() { //app.addToCart()
+			this.cart++ //!!! this
+			//ver. z tutoriala - ignorowało argumenty btw - mountedApp.addToCart("cfg", 1, 2, 3, {a: 456}) -> cart++
+		},
+		
+		addToCartCount(count) {
+			//wersja moja: obsługa argumentów
+			this.cart += count
+			//konsola: mountedApp.addToCartCount(12345)
+		},
+		
+		updateImage(variantImage) {
+			this.image = variantImage
+		}
+	}
 })
