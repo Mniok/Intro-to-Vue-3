9f7c1209508b7449a8ed6ceb007d23b92fe9113b
Mniok <58875711+Mniok@users.noreply.github.com>
2022-07-24 18:33:31 +0200

    L5 - v-for variants and sizes

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+			variants: [
+				{id: 2234, colour:'green'},	//obiekt nie array! żeby mogły być nazwane properties. całość jak array structów
+				{id: 2235, colour:'blue'}	//colour wyświetlamy, id to :key diva generowanego v-forem
+			],
+			sizes: [
+				{id: 2137, size:'XS'},
+				{id: 2138, size:'S'},
+				{id: 2139, size:'M'},
+				{id: 2140, size:'L'},
+				{id: 2141, size:'XL'},
+				{id: 2142, size:'XXL'}
+			]
         }
     }
 })
